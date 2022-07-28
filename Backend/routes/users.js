5ab1207bacdc0a1d3f35a84c8c861e67b0bd9009
anjoos2006 <anjoos20@gmail.com>
2022-07-28 22:40:14 +0530

    Third commit

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken')
 
 //Sign UP
-router.post('/signup',(req,res,next)=>{
-    res.send('signup')
+router.post('/signup',async (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await newUser.save();
+            res.json(savedUser);
+            console.log(savedUser);
+         }catch(err){
+            res.json({ message: err });
+         }   
+                
 })
 
 //Authenticate
