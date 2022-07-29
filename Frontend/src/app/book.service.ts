53f46c16fe2b7b4daf8a295c719c44077afeea08
anjoos2006 <anjoos20@gmail.com>
2022-07-29 22:34:35 +0530

    eigth commit

--- Frontend/src/app/book.service.ts
+++ Frontend/src/app/book.service.ts
@@ -7,9 +7,11 @@ import { HttpClient } from '@angular/common/http';
 export class BookService {
 
   constructor(private http:HttpClient) { }
+  // Get the list of books
   getBooks(){
     return this.http.get("http://localhost:3000/books")
   }
+  // Add new books
   newBooks(nbook: any) {
     return this.http.post("http://localhost:3000/books/add", { "product": nbook })
       .subscribe(data => { console.log(data) })
